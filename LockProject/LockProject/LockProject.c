1e5978ff1b3b895913021646c9c0fbf186897f88
tonyp <trprovencal@gmail.com>
2014-08-18 19:43:29 -0400

    Atmel Studio project folder

---
+++ LockProject/LockProject/LockProject.c
@@ -0,0 +1,17 @@
+/*
+ * LockProject.c
+ *
+ * Created: 8/18/2014 7:41:30 PM
+ *  Author: Tony
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
